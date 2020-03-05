1ba0ab91ac891910ca276de42cbb3e508d7edb0c
Mike Vong <mv910@hotmail.com>
2020-03-04 23:01:31 -0800

    pseudocode app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+// create array of pre-search
+
+// create a search button using jquery
+
+// what happen after the button is push
+
+// ajax pull
+
+    // to search use "q"
+    // to limit it to 10 use "limit=10"
+    // show the rating you want by using "rating=(p,pg,etc)"
+
+// ajaz response
+
+    // still gifs only
+    // show 10 at a time
+    // list them on the page
+    // have premade category
+    // show still
+        //when click gif plays
+    // show title
+    // show rating
+
