ff78be5aef4658be7745dc4a81bee57a0ccf774a
Mike Vong <mv910@hotmail.com>
2020-03-10 18:29:04 -0700

    add css styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,38 @@
 
-var gifArray = ["Cloud Strife ", "Sephiroth", "Tifa Lockhart", "Aerith Gainsborough","Barret Wallace","Yuffie Kisaragi","Red XIII","Vincent Valentine","Cid Highwind"];
+var gifArray = ["Cloud Strife ", "Sephiroth", "Tifa Lockhart","Yuffie Kisaragi","Vincent Valentine","Cid Highwind"];
 
       
 function displayGif() 
     {
     var inputSearch = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=FF7_"+ inputSearch +"&limit=10&api_key=xeDhSZfWtTw8VX5iZ4r1d6wSxdNTEhbH";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ inputSearch +"&limit=10&api_key=xeDhSZfWtTw8VX5iZ4r1d6wSxdNTEhbH";
 
-    $.ajax({
+    $.ajax(
+        {
           url: queryURL,
           method: "GET"
-        }).then(function(response) {
+        })
+        .then(function(response) 
+                {
                 console.log(response);
                 console.log(response.data[0]);
                 $("#movies-view").empty();
                 for (var i = 0; i< response.data.length; i++)
                 {
-                var still = response.data[i].images.fixed_width_still.url;
-                var animate = response.data[i].images.fixed_width.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var animate = response.data[i].images.fixed_height.url;
                 var rating = response.data[i].rating;
-                var title = response.data[i].title;
-                var card = $("<div class='card' style='width; 13rem;'>")
+                var card = $("<div class='card'>")
                 var rate = $("<div class='card-body'> <h5>").text("Rating: " + rating);
-                
-                var img = $("<img class='card-img-top'>");
+                var img = $("<img class='card-img-top' id='pix'>");
                 img.attr("src", still);
                 img.attr("data-still", still);
                 img.attr("data-animate", animate);
                 img.attr("data-state","still");
                 img.addClass("action");
                 card.append(img);
-                card.append(title);
                 card.append(rate);
-                
                 $("#movies-view").append(card);
-
-                
-
-                //$("#movies-view").append("<div class= 'card' style='width: 13rem;'><img id='pix'src=" + still + ">"+"<div class='card-body'>"+"<h5 class='card-title'>"+"Rating: "+rating+"<button class='btn btn-primary'id='switch'>"+"Action"+"</button>"+"</div>"+ "</div>");
-                
                 };
           
             });
@@ -66,7 +60,7 @@ function renderButtons()
         for (var i = 0; i < gifArray.length; i++) 
             {
             var a = $("<button>");
-            a.addClass("gif-btn btn btn-secondary");
+            a.addClass("gif-btn btn btn-warning");
             a.attr("data-name", gifArray[i]);
             a.text(gifArray[i]);
             $("#buttons-view").append(a);
@@ -81,25 +75,5 @@ $("#add-gif").on("click", function(event)
     renderButtons();
     });
 
-//$(document).on("click", ".gif-btn", displayGif);
-
 renderButtons();
-
-
-// ajax pull
-
-    // to search use "q"
-    // to limit it to 10 use "limit=10"
-    // show the rating you want by using "rating=(p,pg,etc)"
-
-// ajaz response
-
-    // still gifs only
-    // show 10 at a time
-    // list them on the page
-    // have premade category
-    // show still
-        //when click gif plays
-    // show title
-    // show rating
 
